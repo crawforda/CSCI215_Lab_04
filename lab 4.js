5f942f2724697bf9baa3f75cfc611d7e5c4b5dcf
crawforda <31389347+crawforda@users.noreply.github.com>
2017-10-01 11:37:11 -0400

    Add files via upload

---
+++ lab 4.js
@@ -0,0 +1,27 @@
+string http://munsellb.people.cofc.edu/img/pound.jpeg
+    <div id="calc">
+    <p class="headerText"> US dollar currancy converstion </p>
+<option value="0.61" selected="selected">British Pound</option>
+<option value="11.73">Estonian Kroon</option>
+<option value="1.11">Canadian Dollar</options>
+<option value="6.06">Chinese Yuan</option>
+<function= convert
+<var= dollar that is initialized to document.getElementsByName("dollars")[0].value
+array http://munsellb.people.cofc.edu/img/euro.jpeg
+array http://munsellb.people.cofc.edu/img/kroon.jpeg
+
+<var= updateExchange>
+function calculate
+<var= dollar>
+<var= exchangeRate>
+var url = 'http://munsellb.people.cofc.edu/img/canadian.jpeg(' + I[index] + ')';
+<function = dollar>
+<var=updateImage>( index );
+<var=exchangeRate>
+
+
+
+
+
+
+
